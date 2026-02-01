8efff213f218bc170b207e0819b768dc62c260bd
web112arch <125330076+web112arch@users.noreply.github.com>
2026-01-31 21:36:40 -0300

    Update BaseStudToken.sol

--- contracts/BaseStudToken.sol
+++ contracts/BaseStudToken.sol
@@ -2,24 +2,53 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BaseStudToken is ERC20, Ownable {
-
+contract BaseStudToken is
+    ERC20,
+    ERC20Capped,
+    ERC20Burnable,
+    Pausable,
+    Ownable
+{
     constructor(
         string memory name_,
         string memory symbol_,
+        uint256 cap_,
         uint256 initialSupply
-    ) ERC20(name_, symbol_) Ownable(msg.sender) {
+    )
+        ERC20(name_, symbol_)
+        ERC20Capped(cap_)
+        Ownable(msg.sender)
+    {
+        require(initialSupply <= cap_, "Initial supply exceeds cap");
         _mint(msg.sender, initialSupply);
     }
 
-    /**
-     * @notice Mint novos tokens (apenas o owner)
-     * @param to endereço que receberá os tokens
-     * @param amount quantidade em wei (18 casas)
-     */
+    /* ========== ADMIN ========== */
+
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /* ========== INTERNAL OVERRIDES ========== */
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Capped) whenNotPaused {
+        super._update(from, to, value);
     }
 }
